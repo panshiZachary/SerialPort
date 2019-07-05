cab2183978cca64b60d5ea292156d8bf6bda50ca
张科 <zhangke8685@adpanshi.com>
2019-07-05 16:45:27 +0800

    add permission

--- app/src/main/java/com/adyun/serialport/Utils.java
+++ app/src/main/java/com/adyun/serialport/Utils.java
@@ -1,12 +1,35 @@
 package com.adyun.serialport;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Zachary
  * on 2019/7/4.
  */
 class Utils {
-    public static void chmod777(File file) {
+    public static boolean chmod777(File file) {
+        if (file == null || !file.exists()){
+            // 文件不存在
+            return false;
+        }
+
+        try {
+            // 获取 root 权限
+            Process su = Runtime.getRuntime().exec("system/bin/su");
+            // 修改文件属性 （ 可读 可写 可执行）
+            String chmod = "chmod 777" + file.getAbsolutePath() + "\n" + "exit\n";
+            su.getOutputStream().write(chmod.getBytes());
+            if (0 == su.waitFor() && file.canRead() && file.canWrite() && file.canExecute()){
+                return true;
+            }
+         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return false;
+
+
     }
 }
